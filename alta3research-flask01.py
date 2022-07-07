19217ad0be445fac3660225930e70f0db952349c
Jordan Surgick <jaysurgjo@users.noreply.github.com>
2022-07-07 21:07:21 +0000

    serving cookies

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -8,6 +8,8 @@ from flask import redirect
 from flask import url_for
 from flask import request
 from flask import render_template
+from flask import jsonify
+
 
 app = Flask(__name__)
 ## This is where we want to redirect users to
@@ -18,4 +20,4 @@ def success(name):
 @app.route("/") # user can land at "/"
 @app.route("/start") # or user can land at "/start"
 def start():
-    return render_template("postmaker.html") # look for templates/postmaker.html
+    return render_template("names.html") # look for templates/postmaker.html
