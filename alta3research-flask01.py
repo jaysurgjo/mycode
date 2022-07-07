a1a3911d0ee0a73beb921a3589cdce17b1df4340
Jordan Surgick <jaysurgjo@users.noreply.github.com>
2022-07-07 19:18:56 +0000

    Jinja templating for HTML and configs

---
+++ alta3research-flask01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Alta3 APIs and HTML"""
+
+## best practice says don't use commas in imports
+# use a single line for each import
+from flask import Flask
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+## This is where we want to redirect users to
+@app.route("/success/<name>")
+def success(name):
+    return f"Welcome {name}\n"
+# This is a landing point for users (a start)
+@app.route("/") # user can land at "/"
+@app.route("/start") # or user can land at "/start"
+def start():
+    return render_template("postmaker.html") # look for templates/postmaker.html
