4ced81518dad9fcd7846684d423b9a5caa5c56b9
Jordan Surgick <jaysurgjo@users.noreply.github.com>
2022-07-08 15:26:10 +0000

    added code and my project

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -13,11 +13,141 @@ from flask import jsonify
 
 app = Flask(__name__)
 ## This is where we want to redirect users to
-@app.route("/success/<name>")
-def success(name):
-    return f"Welcome {name}\n"
-# This is a landing point for users (a start)
-@app.route("/") # user can land at "/"
-@app.route("/start") # or user can land at "/start"
-def start():
-    return render_template("names.html") # look for templates/postmaker.html
+
+classinfo = {
+    "all": [
+        {
+            "name": "Zach",
+            "skill level": "wondrous",
+            "spirit animal": "Alpaca",
+            "super power": "Structure Weakening",
+        },
+        {
+            "name": "Aaron",
+            "skill level": "admirable",
+            "spirit animal": "Donkey",
+            "super power": "Super Strength",
+        },
+        {
+            "name": "Andy",
+            "skill level": "amazing",
+            "spirit animal": "Goat",
+            "super power": "Weather Control",
+        },
+        {
+            "name": "Asif",
+            "skill level": "astonishing",
+            "spirit animal": "Guinea pig",
+            "super power": "Flight",
+        },
+        {
+            "name": "Brent",
+            "skill level": "awesome",
+            "spirit animal": "Horse",
+            "super power": "X-ray Vision",
+        },
+        {
+            "name": "Christopher",
+            "skill level": "brilliant",
+            "spirit animal": "Pig",
+            "super power": "Helicopter Propulsion",
+        },
+        {
+            "name": "Cory",
+            "skill level": "cool",
+            "spirit animal": "Rabbit",
+            "super power": "Invisibility",
+        },
+        {
+            "name": "Ebrima",
+            "skill level": "enjoyable",
+            "spirit animal": "Water buffalo",
+            "super power": "Immobility",
+        },
+        {
+            "name": "Franco",
+            "skill level": "excellent",
+            "spirit animal": "Chicken",
+            "super power": "Immutability",
+        },
+        {
+            "name": "Frank",
+            "skill level": "fabulous",
+            "spirit animal": "Duck",
+            "super power": "Invulnerability",
+        },
+        {
+            "name": "Greg",
+            "skill level": "fantastic",
+            "spirit animal": "Goose",
+            "super power": "Jet Propulsion",
+        },
+        {
+            "name": "Hoon",
+            "skill level": "fine",
+            "spirit animal": "Pigeon",
+            "super power": "Matter Ingestion",
+        },
+        {
+            "name": "JC",
+            "skill level": "incredible",
+            "spirit animal": "Turkey",
+            "super power": "Mobile Invulnerability",
+        },
+        {
+            "name": "Joey",
+            "skill level": "magnificent",
+            "spirit animal": "Aardvark",
+            "super power": "Muscle Manipulation",
+        },
+        {
+            "name": "Jordan",
+            "skill level": "marvelous",
+            "spirit animal": "Aardwolf",
+            "super power": "Nail Manipulation",
+        },
+        {
+            "name": "LB",
+            "skill level": "outstanding",
+            "spirit animal": "Elephant",
+            "super power": "Needle Projection",
+        },
+        {
+            "name": "Mabel",
+            "skill level": "phenomenal",
+            "spirit animal": "Leopard",
+            "super power": "Organic Constructs",
+        },
+        {
+            "name": "Pat",
+            "skill level": "pleasant",
+            "spirit animal": "Albatross",
+            "super power": "Prehensile Hair",
+        },
+        {
+            "name": "Shon",
+            "skill level": "pleasing",
+            "spirit animal": "Alligator",
+            "super power": "Prehensile Tail",
+        },
+    ]
+}
+
+@app.route("/")
+def welcome():
+    return f"Welcome to my page\n"
+
+@app.route("/names")
+def get_names():
+    return jsonify(classinfo)
+
+@app.route("/goodbye")
+def goodbye():
+    return f"Thank you for visiting my page!"
+
+@app.route("/template")
+def render_template():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
---
+++ alta3research-requests02.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""DEMO: receiving JSON"""
+
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import jsonify
+
+app= Flask(__name__)
+
+jordanData= [{
+     "name": "Jay",
+     "realName": "Jordan Surgick",
+     "since": 1987,
+     "powers": [
+         "Super Strength",
+         "Super speed",
+         "Super Handsome",
+         "super human agility"
+               ]
+              }]
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == 'POST':
+        data = request.json
+        if data:
+           data= json.loads(data)
+           name = data["name"]
+           realName = data["realName"]
+           since = data["since"]
+           powers = data["powers"]
+           herodata.append({"name":name,"realName":realName,"since":since,"powers":powers})
+
+    return jsonify(jordanData)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
